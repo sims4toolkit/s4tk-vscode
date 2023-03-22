52379c5973f700b603cab1d1f7ae62a7a4a019c5
Frank Kulak <fs.kulak@gmail.com>
2023-03-21 20:19:34 -0400

    Add support for path aliases.

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
-import { CatScratchEditorProvider } from './editors/catScratchEditor';
-import { PawDrawEditorProvider } from './editors/pawDrawEditor';
+import { CatScratchEditorProvider } from '@editors/catScratchEditor';
+import { PawDrawEditorProvider } from '@editors/pawDrawEditor';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
