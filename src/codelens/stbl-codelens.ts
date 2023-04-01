ac32f79d55725a7e5675763482beb83fb357b587
Frank Kulak <fs.kulak@gmail.com>
2023-04-01 18:05:38 -0400

    Update stbl-codelens.ts

--- src/codelens/stbl-codelens.ts
+++ src/codelens/stbl-codelens.ts
@@ -1,6 +1,8 @@
 import * as vscode from 'vscode';
 import { formatStringKey } from '@s4tk/hashing/formatting';
 
+const _keyRegex = /^\s*"key":[^,]*,/;
+
 export default class StringTableJsonCodeLensProvider implements vscode.CodeLensProvider {
   private _codeLenses: vscode.CodeLens[] = [];
   private _onDidChangeCodeLenses: vscode.EventEmitter<void> = new vscode.EventEmitter<void>();
@@ -42,16 +44,20 @@ export default class StringTableJsonCodeLensProvider implements vscode.CodeLensP
       json = [];
     }
 
-    for (let line = 2, i = 0; line < document.lineCount; line += 4) {
-      const range = new vscode.Range(line, 0, line, 0);
-      const command: vscode.Command = {
-        title: "Copy as XML",
-        tooltip: json[i],
-        command: "s4tk.stringTableJson.copyAsXml",
-        arguments: [json[i]]
-      };
-      this._codeLenses.push(new vscode.CodeLens(range, command));
-      ++i;
+    let stblEntryIndex = 0;
+    for (let lineIndex = 0; lineIndex < document.lineCount; ++lineIndex) {
+      const line = document.lineAt(lineIndex);
+      if (_keyRegex.test(line.text)) {
+        const range = new vscode.Range(lineIndex, 0, lineIndex, 0);
+        const command: vscode.Command = {
+          title: "Copy as XML",
+          tooltip: json[stblEntryIndex],
+          command: "s4tk.stringTableJson.copyAsXml",
+          arguments: [json[stblEntryIndex]]
+        };
+        this._codeLenses.push(new vscode.CodeLens(range, command));
+        ++stblEntryIndex;
+      }
     }
 
     return this._codeLenses;
