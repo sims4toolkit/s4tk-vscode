8f6c6a0d08c3e85367983d3773166e8e581610cf
Frank Kulak <fs.kulak@gmail.com>
2023-05-06 18:10:15 -0400

    Update workspace.ts

--- src/contributions/commands/workspace.ts
+++ src/contributions/commands/workspace.ts
@@ -5,10 +5,12 @@ import S4TKWorkspace from "#workspace/s4tk-workspace";
 export default function registerWorkspaceCommands() {
   vscode.commands.registerCommand(COMMAND.workspace.build, () => {
     // TODO:
+    vscode.window.showInformationMessage("Build");
   });
 
   vscode.commands.registerCommand(COMMAND.workspace.buildDryRun, () => {
     // TODO:
+    vscode.window.showInformationMessage("Dry Run");
   });
 
   vscode.commands.registerCommand(COMMAND.workspace.createWorkspace, () => {
