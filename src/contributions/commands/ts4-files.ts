c3786db103fc5210b3b14aa3e87d6ffa2735cc05
Frank Kulak <fs.kulak@gmail.com>
2023-05-06 14:12:44 -0400

    Move file create logic to #helpers/fs

--- src/contributions/commands/ts4-files.ts
+++ src/contributions/commands/ts4-files.ts
@@ -2,14 +2,13 @@ import * as vscode from "vscode";
 import { StringTableResource } from "@s4tk/models";
 import { COMMAND, EDITOR } from "#constants";
 import StringTableJson from "#models/stbl-json";
-import { fileExists } from "#helpers/fs";
+import { tryCreateCustomFile } from "#helpers/fs";
 
 export default function registerTS4FilesCommands() {
   vscode.commands.registerCommand(COMMAND.ts4Files.createStblBinary, () => {
-    _createNewFile({
+    tryCreateCustomFile({
       promptTitle: "Name of New String Table (Binary)",
-      promptBody: "Enter the name of a STBL to create. You can use slashes to indicate subfolders.",
-      extension: ".stbl",
+      fileExtension: ".stbl",
       contentGenerator: () => (new StringTableResource()).getBuffer(),
       launchFile: (uri) => vscode.commands.executeCommand(
         'vscode.openWith',
@@ -20,45 +19,11 @@ export default function registerTS4FilesCommands() {
   });
 
   vscode.commands.registerCommand(COMMAND.ts4Files.createStblJson, () => {
-    _createNewFile({
+    tryCreateCustomFile({
       promptTitle: "Name of New String Table (JSON)",
-      promptBody: "Enter the name of a STBL to create. You can use slashes to indicate subfolders.",
-      extension: ".stbl.json",
+      fileExtension: ".stbl.json",
       contentGenerator: () => StringTableJson.generateBuffer("object"),
       launchFile: (uri) => vscode.window.showTextDocument(uri),
     });
   });
 }
-
-//#region Helpers
-
-async function _createNewFile(options: {
-  promptTitle: string;
-  promptBody: string;
-  extension: string;
-  contentGenerator: () => Uint8Array;
-  launchFile: (uri: vscode.Uri) => void;
-}) {
-  const workspaceFolders = vscode.workspace.workspaceFolders;
-  if (!workspaceFolders) {
-    vscode.window.showErrorMessage("Creating a new file requires opening a workspace");
-    return;
-  }
-
-  let filename = await vscode.window.showInputBox({
-    title: options.promptTitle,
-    prompt: options.promptBody
-  });
-
-  if (!filename) return;
-  if (!filename.endsWith(options.extension)) filename = filename + options.extension;
-  const uri = vscode.Uri.joinPath(workspaceFolders[0].uri, filename);
-
-  if (!(await fileExists(uri))) {
-    await vscode.workspace.fs.writeFile(uri, options.contentGenerator());
-  }
-
-  options.launchFile(uri);
-}
-
-//#endregion
--- src/core/helpers/fs.ts
+++ src/core/helpers/fs.ts
@@ -1,6 +1,40 @@
 import * as vscode from "vscode";
 
 /**
+ * Prompts the user for a file name and attempts to create it using the given
+ * content generator. If the 
+ * 
+ * @param options Options for prompting the user and creating the document
+ */
+export async function tryCreateCustomFile(options: {
+  promptTitle: string;
+  fileExtension: string;
+  contentGenerator: () => Uint8Array;
+  launchFile: (uri: vscode.Uri) => void;
+}) {
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+  if (!workspaceFolders) {
+    vscode.window.showErrorMessage("Creating a new file requires opening a workspace.");
+    return;
+  }
+
+  let filename = await vscode.window.showInputBox({
+    title: options.promptTitle,
+    prompt: "File path is relative to the root of your profect. Use slashes to indicate subfolders.",
+  });
+
+  if (!filename) return;
+  if (!filename.endsWith(options.fileExtension))
+    filename = filename + options.fileExtension;
+
+  const uri = vscode.Uri.joinPath(workspaceFolders[0].uri, filename);
+  if (!(await fileExists(uri)))
+    await vscode.workspace.fs.writeFile(uri, options.contentGenerator());
+
+  options.launchFile(uri);
+}
+
+/**
  * Returns whether or not a file exists at the given URI.
  * 
  * @param uri URI to check
