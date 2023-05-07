0424030cc1e7c55a9c6f1db6615d902739b907fe
Frank Kulak <fs.kulak@gmail.com>
2023-05-06 20:22:02 -0400

    Update s4tk-workspace.ts

--- src/core/workspace/s4tk-workspace.ts
+++ src/core/workspace/s4tk-workspace.ts
@@ -1,28 +1,19 @@
 import * as vscode from "vscode";
-import { CONTEXT, FILENAME } from "#constants";
-import { S4TKConfig } from "#models/s4tk-config";
-import { fileExists, findOpenDocument, replaceEntireDocument } from "#helpers/fs";
-import { MessageButton, handleMessageButtonClick } from "./messaging";
-import StringTableJson from "#models/stbl-json";
 import { SCHEMA_DEFAULTS } from "#assets";
+import { CONTEXT, FILENAME } from "#constants";
+import { fileExists, findOpenDocument, replaceEntireDocument } from "#helpers/fs";
+import { S4TKConfig } from "#models/s4tk-config";
+import StringTableJson from "#models/stbl-json";
+import { MessageButton, handleMessageButtonClick } from "./messaging";
 
 class _S4TKWorkspace {
   //#region Properties
 
   private _isSavingDocument: boolean = false;
-
-  private _config?: S4TKConfig;
-  get config() { return this._config; }
-  private set config(config: S4TKConfig | undefined) {
-    this._config = config;
-    vscode.commands.executeCommand(
-      'setContext',
-      CONTEXT.workspace.active,
-      this.active
-    );
-  }
-
-  get active() { return Boolean(this._config); }
+  private _activeConfig?: S4TKConfig;
+  private _blankConfig: S4TKConfig = S4TKConfig.blankProxy();
+  get config(): S4TKConfig { return this._activeConfig ?? this._blankConfig; }
+  get active() { return Boolean(this._activeConfig); }
 
   //#endregion
 
@@ -40,7 +31,7 @@ class _S4TKWorkspace {
     vscode.workspace.onDidDeleteFiles((e) => {
       if (!this.active) return;
       if (e.files.some(uri => uri.path.endsWith(FILENAME.config))) {
-        this.config = undefined;
+        this._setConfig();
         vscode.window.showWarningMessage("S4TK config has been unloaded.");
       }
     });
@@ -58,11 +49,10 @@ class _S4TKWorkspace {
    * used to make the edit. If not, then it will be written straight to disk.
    */
   async addPackageInstructions(editor?: vscode.TextEditor) {
-    await this._tryEditAndSaveConfig("Add Package Instructions", editor, (config) => {
-      config.buildInstructions.packages ??= [];
+    await this._tryEditAndSaveConfig("Add Package", editor, (config) => {
       config.buildInstructions.packages.push({
-        filename: "",
-        include: [],
+        filename: `MyPackage${config.buildInstructions.packages.length + 1}`,
+        include: ["**/*"],
       });
     });
   }
@@ -95,7 +85,8 @@ class _S4TKWorkspace {
 
     const stblUri = vscode.Uri.joinPath(rootUri, "strings", "default.stbl.json");
     if (!(await fileExists(stblUri))) {
-      vscode.workspace.fs.writeFile(stblUri, StringTableJson.generateBuffer("object"));
+      const stblType = this.config.workspaceSettings.newStringTableJsonType;
+      vscode.workspace.fs.writeFile(stblUri, StringTableJson.generateBuffer(stblType));
     }
   }
 
@@ -120,8 +111,7 @@ class _S4TKWorkspace {
    * if there is no config to load
    */
   async loadConfig({ showNoConfigError = false }: { showNoConfigError?: boolean; } = {}) {
-    // do not use _config or delete; must use setter to trigger context change
-    this.config = undefined;
+    this._setConfig();
 
     const configInfo = await S4TKConfig.find();
     if (!(configInfo.uri && configInfo.exists)) {
@@ -137,8 +127,7 @@ class _S4TKWorkspace {
       const content = await vscode.workspace.fs.readFile(configInfo.uri!);
       const config = S4TKConfig.parse(content.toString());
       vscode.window.showInformationMessage('Successfully loaded S4TK config.');
-      // do not use _config; must use setter to trigger context change
-      this.config = config;
+      this._setConfig(config);
     } catch (e) {
       vscode.window.showErrorMessage(
         `Could not validate S4TK config. You will not be able to build your project until all errors are resolved and the config has been reloaded. (${e})`,
@@ -155,9 +144,7 @@ class _S4TKWorkspace {
    */
   async setDefaultStbl(stblUri: vscode.Uri) {
     await this._tryEditAndSaveConfig("Set Default STBL", null, (config) => {
-      //@ts-ignore Ok to leave blank, proxy takes care of defaults
-      config.stringTables ??= {};
-      config.stringTables.defaultPath = stblUri.fsPath;
+      config.workspaceSettings.defaultStringTable = stblUri.fsPath;
     });
   }
 
@@ -165,17 +152,26 @@ class _S4TKWorkspace {
 
   //#region Private Methods
 
+  private _setConfig(config?: S4TKConfig) {
+    this._activeConfig = config;
+    vscode.commands.executeCommand(
+      'setContext',
+      CONTEXT.workspace.active,
+      this.active
+    );
+  }
+
   private async _tryEditAndSaveConfig(
     action: string,
     editor: vscode.TextEditor | undefined | null,
     fn: (config: S4TKConfig) => void
   ) {
-    if (!this._config) {
+    if (!this.active) {
       vscode.window.showErrorMessage(
         `Cannot perform '${action}' because no S4TK config is currently loaded.`,
         MessageButton.ReloadConfig,
       ).then(handleMessageButtonClick);
-      return undefined;
+      return;
     }
 
     const configUri = (await S4TKConfig.find()).uri;
@@ -184,14 +180,14 @@ class _S4TKWorkspace {
         `Cannot perform '${action}' because no S4TK config could be located. Please report this problem.`,
         MessageButton.ReportProblem,
       ).then(handleMessageButtonClick);
-      return undefined;
+      return;
     }
 
     const openConfigDocument = editor?.document ?? findOpenDocument(configUri);
     if (openConfigDocument)
       await this.trySaveDocumentAndReload(openConfigDocument);
 
-    if (!this._config) {
+    if (!this._activeConfig) {
       vscode.window.showErrorMessage(
         `Your S4TK config file was automatically saved before performing '${action}', and these changes have made it invalid. You must fix your config file and reload it before trying '${action}' again.`,
         MessageButton.ReloadConfig,
@@ -199,11 +195,8 @@ class _S4TKWorkspace {
       return undefined;
     }
 
-    S4TKConfig.modify(this._config, fn);
-    const newContent = S4TKConfig.stringify(this._config);
-
-    // important to save because if user undoes changes before saving then the
-    // config will fall out of sync
+    S4TKConfig.modify(this._activeConfig, fn);
+    const newContent = S4TKConfig.stringify(this._activeConfig);
     if (!(editor && await replaceEntireDocument(editor, newContent, true)))
       vscode.workspace.fs.writeFile(configUri, Buffer.from(newContent));
   }
@@ -211,5 +204,8 @@ class _S4TKWorkspace {
   //#endregion
 }
 
+/**
+ * Manages the state of the S4TK workspace, including the config.
+ */
 const S4TKWorkspace = new _S4TKWorkspace();
 export default S4TKWorkspace;
