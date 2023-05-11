4139f9c97af8e870093fc645909d8d152151c041
Frank Kulak <fs.kulak@gmail.com>
2023-05-11 11:24:35 -0400

    Update builder.ts

--- src/core/building/builder.ts
+++ src/core/building/builder.ts
@@ -73,83 +73,51 @@ function _buildPackage(context: PackageBuildContext): models.Package {
   context.filepaths.forEach(filepath => {
     const buffer = fs.readFileSync(filepath);
 
-    if (buffer.slice(0, 4).toString() === "DBPF") {
-      models.Package.extractResources(buffer).forEach((entry) => {
-        if (entry.key.type === enums.BinaryResourceType.StringTable) {
-          context.stbls.push(entry as types.ResourceKeyPair<models.StringTableResource>);
-        } else {
-          // TODO: add to build summary
-          context.pkg.add(entry.key, entry.value);
-        }
-      });
-      return;
-    }
+    if (_tryAddPackage(context, filepath, buffer)) return;
+    if (_tryAddTgiFile(context, filepath, buffer)) return;
 
     // TODO: add to build summary if adding to package
     // context.pkgInfo.resources.push
 
-    const tgiKey = parseKeyFromTgi(filepath);
-    if (tgiKey) {
-      if (tgiKey.type === enums.BinaryResourceType.SimData) {
-        if (buffer.slice(0, 4).toString() === "DATA") {
-          context.pkg.add(tgiKey, models.RawResource.from(buffer));
-        } else {
-          context.pkg.add(tgiKey, models.SimDataResource.fromXml(buffer));
-        }
-      } else if (tgiKey.type === enums.BinaryResourceType.StringTable) {
-        if (buffer.slice(0, 4).toString() === "STBL") {
-          const stbl = models.StringTableResource.from(buffer);
-          context.stbls.push({ key: tgiKey, value: stbl });
-        } else {
-          const stblJson = StringTableJson.parse(buffer.toString());
-          context.stbls.push({ key: tgiKey, value: stblJson.toBinaryResource() });
-        }
+    const basename = path.basename(filepath);
+    const extname = path.extname(basename);
+
+    if (extname === ".xml") {
+      if (basename.endsWith(".SimData.xml")) {
+        // TODO:
       } else {
-        // intentionally not checking for tuning or adding filepath to tuning
-        // map, because no SimData is going to have the same path if it has TGI
-        context.pkg.add(tgiKey, models.RawResource.from(buffer));
+        // TODO:
       }
+    } else if (extname === ".json") {
+      // assuming STBL JSON for now, as no other JSONs pass the filter
+      // TODO: add warning if random instance is being used
+      const stblJson = StringTableJson.parse(buffer.toString());
+      context.stbls.push({
+        key: stblJson.getResourceKey(S4TKWorkspace.defaultLocale),
+        value: stblJson.toBinaryResource()
+      });
+    } else if (extname === ".stbl") {
+      // TODO: add warning that random instance is being used
+      context.stbls.push({
+        key: {
+          type: enums.BinaryResourceType.StringTable,
+          group: 0x80000000,
+          instance: enums.StringTableLocale.setHighByte(
+            enums.StringTableLocale[S4TKWorkspace.defaultLocale],
+            randomFnv64()
+          )
+        },
+        value: models.StringTableResource.from(buffer),
+      });
     } else {
-      const basename = path.basename(filepath);
-      const extname = path.extname(basename);
+      const warning = "File could not be resolved as a TS4 resource. This error should never occur. If you are reading this, please report it.";
 
-      if (extname === ".xml") {
-        if (basename.endsWith(".SimData.xml")) {
-          // TODO:
-        } else {
-          // TODO:
-        }
-      } else if (extname === ".json") {
-        // assuming STBL JSON for now, as no other JSONs pass the filter
-        // TODO: add warning if random instance is being used
-        const stblJson = StringTableJson.parse(buffer.toString());
-        context.stbls.push({
-          key: stblJson.getResourceKey(S4TKWorkspace.defaultLocale),
-          value: stblJson.toBinaryResource()
-        });
-      } else if (extname === ".stbl") {
-        // TODO: add warning that random instance is being used
-        context.stbls.push({
-          key: {
-            type: enums.BinaryResourceType.StringTable,
-            group: 0x80000000,
-            instance: enums.StringTableLocale.setHighByte(
-              enums.StringTableLocale[S4TKWorkspace.defaultLocale],
-              randomFnv64()
-            )
-          },
-          value: models.StringTableResource.from(buffer),
-        });
-      } else {
-        const warning = "File could not be resolved as a TS4 resource. This error should never occur. If you are reading this, please report it.";
+      context.summary.written.fileWarnings.push({
+        file: BuildSummary.makeRelative(context.summary, filepath),
+        warnings: [warning],
+      });
 
-        context.summary.written.fileWarnings.push({
-          file: BuildSummary.makeRelative(context.summary, filepath),
-          warnings: [warning],
-        });
-
-        throw FatalBuildError(warning);
-      }
+      throw FatalBuildError(warning);
     }
   });
 
@@ -158,17 +126,55 @@ function _buildPackage(context: PackageBuildContext): models.Package {
   return context.pkg;
 }
 
-function _parsePackage(summary: BuildSummary, filepath: string): types.ResourceKeyPair[] {
-  // TODO: update summary
+function _tryAddPackage(context: PackageBuildContext, filepath: string, buffer: Buffer): boolean {
   try {
-    // TODO: check if any keys overlap with existing ones
-    const buffer = fs.readFileSync(filepath);
-    return models.Package.extractResources(buffer);
+    if (buffer.slice(0, 4).toString() === "DBPF") {
+      models.Package.extractResources(buffer).forEach((entry) => {
+        if (entry.key.type === enums.BinaryResourceType.StringTable) {
+          context.stbls.push(entry as types.ResourceKeyPair<models.StringTableResource>);
+        } else {
+          // TODO: push to context.pkgInfo.resources
+          context.pkg.add(entry.key, entry.value);
+        }
+      });
+
+      return true;
+    }
+
+    return false;
   } catch (e) {
     throw FatalBuildError(
       `Failed to extract resources from Package (${filepath}) [${e}]`
     );
   }
+}
+
+function _tryAddTgiFile(context: PackageBuildContext, filepath: string, buffer: Buffer): boolean {
+  const tgiKey = parseKeyFromTgi(filepath);
+  if (!tgiKey) return false;
+
+  if (tgiKey.type === enums.BinaryResourceType.SimData) {
+    // TODO: push to context.pkgInfo.resources
+    if (buffer.slice(0, 4).toString() === "DATA") {
+      context.pkg.add(tgiKey, models.RawResource.from(buffer));
+    } else {
+      context.pkg.add(tgiKey, models.SimDataResource.fromXml(buffer));
+    }
+  } else if (tgiKey.type === enums.BinaryResourceType.StringTable) {
+    // TODO: push to context.pkgInfo.resources
+    if (buffer.slice(0, 4).toString() === "STBL") {
+      const stbl = models.StringTableResource.from(buffer);
+      context.stbls.push({ key: tgiKey, value: stbl });
+    } else {
+      const stblJson = StringTableJson.parse(buffer.toString());
+      context.stbls.push({ key: tgiKey, value: stblJson.toBinaryResource() });
+    }
+  } else {
+    // TODO: push to context.pkgInfo.resources
+    context.pkg.add(tgiKey, models.RawResource.from(buffer));
+  }
+
+  return true;
 }
 
 function _parseStblBinary(summary: BuildSummary, filepath: string): types.ResourceKeyPair {
