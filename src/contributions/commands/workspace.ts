e9e34a032b5f2c00a92779e7cf23441da1e088df
Frank Kulak <fs.kulak@gmail.com>
2023-05-12 14:57:30 -0400

    Update workspace.ts

--- src/contributions/commands/workspace.ts
+++ src/contributions/commands/workspace.ts
@@ -33,46 +33,8 @@ export default function registerWorkspaceCommands() {
     S4TKWorkspace.setDefaultStbl(uri);
   });
 
-  vscode.commands.registerCommand(COMMAND.workspace.addNewString, async (clickedUri?: vscode.Uri) => {
-    try {
-      let uri = clickedUri;
-      if (!uri) {
-        if (!S4TKWorkspace.defaultStringTable) {
-          vscode.window.showWarningMessage("Cannot add string because no default string table is set in s4tk.config.json.");
-          return;
-        }
-
-        uri = vscode.Uri.parse(S4TKConfig.resolvePath(S4TKWorkspace.defaultStringTable)!);
-      }
-
-      try {
-        const bytes = await vscode.workspace.fs.readFile(uri);
-        var stbl = new StringTableProxy(bytes);
-      } catch (e) {
-        vscode.window.showErrorMessage(`Path '${uri.fsPath}' does not lead to a valid string table.`);
-        return;
-      }
-
-      const input = await vscode.window.showInputBox({
-        title: "Enter String Text",
-        prompt: "A random FNV32 will be generated for the key.",
-      });
-
-      if (!input) return;
-      const key = stbl.addValue(input);
-      await vscode.workspace.fs.writeFile(uri, stbl.serialize());
-
-      const clickToCopy = "Copy as XML";
-      vscode.window.showInformationMessage(
-        `Added new string to ${path.basename(uri.fsPath)}`,
-        clickToCopy,
-      ).then(value => {
-        if (value === clickToCopy)
-          vscode.env.clipboard.writeText(`${formatStringKey(key)}<!--${input}-->`);
-      });
-    } catch (e) {
-      vscode.window.showErrorMessage(`Could not add string to STBL [${e}]`);
-    }
+  vscode.commands.registerCommand(COMMAND.workspace.addNewString, (clickedUri?: vscode.Uri) => {
+    _addNewString(clickedUri);
   });
 }
 
@@ -88,7 +50,15 @@ async function _runBuild(mode: BuildMode, readableMode: string) {
     const buildSummaryUri = await _outputBuildSummary(summary);
 
     if (summary.buildInfo.success) {
-      vscode.window.showInformationMessage(`S4TK ${readableMode} Successful`);
+      const warnings: string[] = [];
+      const warnIf = (num: number, msg: string) => {
+        if (num) warnings.push(`${num} ${msg}${(num === 1) ? '' : 's'}`);
+      }
+      warnIf(summary.buildInfo.problems, "problem");
+      warnIf(summary.written.ignoredSourceFiles.length, "ignored file");
+      warnIf(summary.written.missingSourceFiles.length, "missing file");
+      const warningMsg = warnings.length ? ` [${warnings.join("; ")}]` : "";
+      vscode.window.showInformationMessage(`S4TK ${readableMode} Successful${warningMsg}`);
     } else if (buildSummaryUri) {
       const viewBuildSummary = 'View BuildSummary.json';
       vscode.window.showErrorMessage(
@@ -112,4 +82,46 @@ async function _outputBuildSummary(summary: BuildSummary): Promise<vscode.Uri | 
   const content = JSON.stringify(summary, null, S4TKWorkspace.spacesPerIndent);
   await vscode.workspace.fs.writeFile(uri, Buffer.from(content));
   return uri;
+}
+
+async function _addNewString(clickedUri?: vscode.Uri) {
+  try {
+    let uri = clickedUri;
+    if (!uri) {
+      if (!S4TKWorkspace.defaultStringTable) {
+        vscode.window.showWarningMessage("Cannot add string because no default string table is set in s4tk.config.json.");
+        return;
+      }
+
+      uri = vscode.Uri.parse(S4TKConfig.resolvePath(S4TKWorkspace.defaultStringTable)!);
+    }
+
+    try {
+      const bytes = await vscode.workspace.fs.readFile(uri);
+      var stbl = new StringTableProxy(bytes);
+    } catch (e) {
+      vscode.window.showErrorMessage(`Path '${uri.fsPath}' does not lead to a valid string table.`);
+      return;
+    }
+
+    const input = await vscode.window.showInputBox({
+      title: "Enter String Text",
+      prompt: "A random FNV32 will be generated for the key.",
+    });
+
+    if (!input) return;
+    const key = stbl.addValue(input);
+    await vscode.workspace.fs.writeFile(uri, stbl.serialize());
+
+    const clickToCopy = "Copy as XML";
+    vscode.window.showInformationMessage(
+      `Added new string to ${path.basename(uri.fsPath)}`,
+      clickToCopy,
+    ).then(value => {
+      if (value === clickToCopy)
+        vscode.env.clipboard.writeText(`${formatStringKey(key)}<!--${input}-->`);
+    });
+  } catch (e) {
+    vscode.window.showErrorMessage(`Could not add string to STBL [${e}]`);
+  }
 }
