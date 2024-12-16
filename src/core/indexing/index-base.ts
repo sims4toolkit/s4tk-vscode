9e4058e4cfbb75caf106b31f641ae5f9c6d4a67e
frankkulak <fs.kulak@gmail.com>
2024-12-16 15:25:21 -0500

    Refactor tuning index to use index base class.

---
+++ src/core/indexing/index-base.ts
@@ -0,0 +1,102 @@
+import * as glob from "glob";
+import * as vscode from "vscode";
+import { resolveGlobPattern } from "#helpers/fs";
+
+/**
+ * Keeps track of files of a specific type within a workspace.
+ */
+export default abstract class IndexBase implements vscode.Disposable {
+  private _watcherDisposables: vscode.Disposable[] = [];
+
+  //#region Lifecycle
+
+  constructor(
+    private readonly _globPatterns: string[],
+    private _sourceFolder: vscode.Uri | undefined
+  ) {
+    this._startFsWatchers();
+  }
+
+  dispose() {
+    this._teardownFsWatchers();
+  }
+
+  //#endregion
+
+  //#region Public Methods
+
+  /**
+   * Clears all data in the index and re-indexes the source folder.
+   */
+  refresh() {
+    this._clearIndex();
+    this._indexSourceFolder();
+  }
+
+  /**
+   * Replaces the source folder and refreshes the index.
+   * 
+   * @param uri URI to new source folder
+   */
+  updateSourceFolder(uri: vscode.Uri) {
+    this._teardownFsWatchers();
+    this._sourceFolder = uri;
+    this._startFsWatchers();
+    this.refresh();
+  }
+
+  //#endregion
+
+  //#region Protected Abstract Helpers
+
+  protected abstract _clearIndex(): void;
+
+  protected abstract _onFileChanged(uri: vscode.Uri): void;
+
+  protected abstract _onFileCreated(uri: vscode.Uri): void;
+
+  protected abstract _onFileDeleted(uri: vscode.Uri): void;
+
+  protected abstract _shouldIndexFile(filepath: string): boolean;
+
+  protected abstract _indexFile(uri: vscode.Uri): void;
+
+  //#endregion
+
+  //#region Private Helpers
+
+  private async _indexSourceFolder() {
+    if (!this._sourceFolder) return;
+    for (const globPattern in this._globPatterns) {
+      const resolvedPattern = resolveGlobPattern(this._sourceFolder, globPattern);
+      glob.sync(resolvedPattern).forEach(filepath => {
+        try {
+          if (!this._shouldIndexFile(filepath)) return;
+          const uri = vscode.Uri.file(filepath);
+          this._indexFile(uri);
+        } catch (e) {
+          console.error(`Error while indexing '${filepath}':\n${e}`);
+        }
+      });
+    }
+  }
+
+  private _startFsWatchers() {
+    if (!this._sourceFolder) return;
+    for (const globPattern in this._globPatterns) {
+      const fsPattern = new vscode.RelativePattern(this._sourceFolder, globPattern);
+      const watcher = vscode.workspace.createFileSystemWatcher(fsPattern);
+      watcher.onDidChange(e => this._onFileChanged(e), this, this._watcherDisposables);
+      watcher.onDidCreate(e => this._onFileCreated(e), this, this._watcherDisposables);
+      watcher.onDidDelete(e => this._onFileDeleted(e), this, this._watcherDisposables);
+      this._watcherDisposables.push(watcher);
+    }
+  }
+
+  private _teardownFsWatchers() {
+    while (this._watcherDisposables.length)
+      this._watcherDisposables.pop()?.dispose();
+  }
+
+  //#endregion
+}
--- src/core/indexing/tuning-index.ts
+++ src/core/indexing/tuning-index.ts
@@ -1,43 +1,25 @@
 import * as vscode from "vscode";
-import { sync as globSync } from "glob";
-import { resolveGlobPattern } from "#helpers/fs";
-import type { TuningMetadata } from "./types";
-import { inferKeyFromMetadata, inferTuningMetadata } from "./inference";
 import { sanitizeXmlComment } from "#helpers/xml";
+import { inferKeyFromMetadata, inferTuningMetadata } from "./inference";
+import { TuningMetadata } from "./types";
+import IndexBase from "./index-base";
 
 /**
- * Keeps track of all tuning resources within a workspace's source folder.
+ * Keeps track of tuning files within a workspace.
  */
-export default class TuningIndex implements vscode.Disposable {
-  private _watcherDisposables: vscode.Disposable[] = [];
+export default class TuningIndex extends IndexBase {
   private _pathsToDefinitions = new Map<string, TuningMetadata>();
   private _instancesToPaths = new Map<string, string[]>();
 
   //#region Lifecycle
 
-  constructor(private _sourceFolder?: vscode.Uri) {
-    this._startFsWatcher();
-  }
-
-  dispose() {
-    this._teardownFsWatcher();
+  constructor(sourceFolder?: vscode.Uri) {
+    super(["**/*.xml"], sourceFolder);
   }
 
   //#endregion
 
   //#region Public Methods
-
-  /**
-   * Replaces the source folder and refreshes the index.
-   * 
-   * @param uri URI to new source folder
-   */
-  updateSourceFolder(uri: vscode.Uri) {
-    this._teardownFsWatcher();
-    this._sourceFolder = uri;
-    this._startFsWatcher();
-    this.refresh();
-  }
 
   /**
    * Returns a list of all metadata for every tracked tuning file.
@@ -94,33 +76,53 @@ export default class TuningIndex implements vscode.Disposable {
     return fsPaths ? Boolean(fsPaths.length > 1) : false;
   }
 
-  /**
-   * Clears all data in the index and re-indexes the source folder.
-   */
-  refresh() {
-    this._clearIndex();
-    this._indexSourceFolder();
-  }
-
   //#endregion
 
-  //#region Private Helpers
+  //#region Implemented Abstract Methods
 
-  private _clearIndex() {
+  protected _clearIndex() {
     this._pathsToDefinitions.clear();
     this._instancesToPaths.clear();
   }
 
-  private async _indexSourceFolder() {
-    if (!this._sourceFolder) return;
-    const pattern = resolveGlobPattern(this._sourceFolder, "**/*.xml");
-    globSync(pattern).forEach(filepath => {
-      if (filepath.endsWith(".SimData.xml")) return;
-      const uri = vscode.Uri.file(filepath);
-      const metadata = inferTuningMetadata(uri);
-      this._pathsToDefinitions.set(uri.fsPath, metadata);
-      if (metadata.attrs?.s) this._onIdAdded(metadata.attrs.s, uri.fsPath);
-    });
+  protected _onFileChanged(uri: vscode.Uri) {
+    this._onFileUpdated(uri);
+  }
+
+  protected _onFileCreated(uri: vscode.Uri) {
+    this._onFileUpdated(uri);
+  }
+
+  protected _onFileDeleted(uri: vscode.Uri) {
+    const definition = this._pathsToDefinitions.get(uri.fsPath);
+    if (!definition) return;
+    this._pathsToDefinitions.delete(uri.fsPath);
+    if (definition.attrs?.s == undefined) return;
+    this._onIdRemoved(definition.attrs.s, uri.fsPath);
+  }
+
+  protected _shouldIndexFile(filepath: string): boolean {
+    return !filepath.endsWith(".SimData.xml");
+  }
+
+  protected _indexFile(uri: vscode.Uri): void {
+    const metadata = inferTuningMetadata(uri);
+    this._pathsToDefinitions.set(uri.fsPath, metadata);
+    if (metadata.attrs?.s) this._onIdAdded(metadata.attrs.s, uri.fsPath);
+  }
+
+  //#endregion
+
+  //#region Private Methods
+
+  private _onFileUpdated(uri: vscode.Uri) {
+    if (uri.fsPath.endsWith(".SimData.xml")) return;
+    const oldId = this._pathsToDefinitions.get(uri.fsPath)?.attrs?.s;
+    if (oldId && this._instancesToPaths.has(oldId))
+      this._onIdRemoved(oldId, uri.fsPath);
+    const metadata = inferTuningMetadata(uri);
+    this._pathsToDefinitions.set(uri.fsPath, metadata);
+    if (metadata.attrs?.s) this._onIdAdded(metadata.attrs.s, uri.fsPath);
   }
 
   private _onIdAdded(id: string, filepath: string) {
@@ -141,39 +143,6 @@ export default class TuningIndex implements vscode.Disposable {
         if (index >= 0) fsPaths.splice(index, 1);
       }
     }
-  }
-
-  private _updateFile(uri: vscode.Uri) {
-    if (uri.fsPath.endsWith(".SimData.xml")) return;
-    const oldId = this._pathsToDefinitions.get(uri.fsPath)?.attrs?.s;
-    if (oldId && this._instancesToPaths.has(oldId))
-      this._onIdRemoved(oldId, uri.fsPath);
-    const metadata = inferTuningMetadata(uri);
-    this._pathsToDefinitions.set(uri.fsPath, metadata);
-    if (metadata.attrs?.s) this._onIdAdded(metadata.attrs.s, uri.fsPath);
-  }
-
-  private _removeFile(uri: vscode.Uri) {
-    const definition = this._pathsToDefinitions.get(uri.fsPath);
-    if (!definition) return;
-    this._pathsToDefinitions.delete(uri.fsPath);
-    if (definition.attrs?.s == undefined) return;
-    this._onIdRemoved(definition.attrs.s, uri.fsPath);
-  }
-
-  private _startFsWatcher() {
-    if (!this._sourceFolder) return;
-    const pattern = new vscode.RelativePattern(this._sourceFolder, "**/*.xml");
-    const watcher = vscode.workspace.createFileSystemWatcher(pattern);
-    watcher.onDidChange(e => this._updateFile(e), this, this._watcherDisposables);
-    watcher.onDidCreate(e => this._updateFile(e), this, this._watcherDisposables);
-    watcher.onDidDelete(e => this._removeFile(e), this, this._watcherDisposables);
-    this._watcherDisposables.push(watcher);
-  }
-
-  private _teardownFsWatcher() {
-    while (this._watcherDisposables.length)
-      this._watcherDisposables.pop()?.dispose();
   }
 
   //#endregion
